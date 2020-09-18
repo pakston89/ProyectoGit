3f7739e49bba81569c7d9480900d965398622bae
Pau <Pau@DESKTOP-JFIJL69>
2020-09-18 13:40:56 +0200

    se crea clase Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public String getHello() {
+        return Hello;
+    }
+
+    public void setHello(String hello) {
+        Hello = hello;
+    }
+
+    String Hello;
+}
