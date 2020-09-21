f2370959d26e9dfa8c16ba5927007ef4db2d2772
Pau <pauserratosa5@gmail.com>
2020-09-21 15:29:24 +0200

    Revert "se ignoran archivos .conf"
    This reverts commit f53cb886de5d8600c9f57588b1670b61c21912b7.

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    String Hello;
+    String Adios;
+    String kpasaTio33;
+
+    public String getHello() {
+        return Hello;
+    }
+
+    public void setHello(String hello) {
+        Hello = hello;
+    }
+}
